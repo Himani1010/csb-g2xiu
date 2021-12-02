aad0e94feb83a49acb5cedf4883ff637a4b2251e
Code Sandbox <hello@codesandbox.io>
2021-12-02 16:41:12 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.c447bf125c782f9a935884f2942cfcb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c488f3f5a60e9bb02337e4252d34962",
+    "url": "https://himani1010.github.io/csb-g2xiu/index.html"
+  },
+  {
+    "revision": "037105d51a67e8d863aa",
+    "url": "https://himani1010.github.io/csb-g2xiu/static/css/main.c41ee3b4.chunk.css"
+  },
+  {
+    "revision": "55a29c9394d48763cc90",
+    "url": "https://himani1010.github.io/csb-g2xiu/static/js/2.ebedfd81.chunk.js"
+  },
+  {
+    "revision": "464fef3691779b3926634327df24dae8",
+    "url": "https://himani1010.github.io/csb-g2xiu/static/js/2.ebedfd81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "037105d51a67e8d863aa",
+    "url": "https://himani1010.github.io/csb-g2xiu/static/js/main.67d1de34.chunk.js"
+  },
+  {
+    "revision": "8825d72934f7bbfd38d0",
+    "url": "https://himani1010.github.io/csb-g2xiu/static/js/runtime-main.be39bc93.js"
+  }
+]);
